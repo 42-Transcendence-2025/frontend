d5727c7a8d09492dd899d25031d71ca7cce6eba8
Sandoramix <sandoramix.dev@gmail.com>
2025-01-06 18:00:25 +0100

    Refactor app.js to centralize config settings, enhance routing structure, and improve locale handling. Consistent variable naming applied

--- app.js
+++ app.js
@@ -10,67 +10,93 @@ import TestController from "./src/controllers/testCtrl.js";
  * 	titleSuffix: string,
  * 	init: function,
  * }} Controller
- */
+*/
 
 //-----------------------------------------------------------------------------
-// GLOBAL VARIABLES
-
-var LOCAL_STORAGE_KEYS = {
-	locale: "locale",
-};
-
-const LOCALE_URLS = {
-	en:"i18n/en.json",
-	it:"i18n/it.json",
-	uk:"i18n/uk.json",
-};
-const LOCALE_IMAGES = {
-	en: "assets/flags/us.png",
-	it: "assets/flags/it.png",
-	uk: "assets/flags/ua.png",
-}
-const LOCALE_SWITCH_ID = "locale-switch";
-
-const APP_BASE_TITLE = "Pong Game";
-const APP_CONTAINER_ID = "app";
-
+// GLOBAL SETTINGS
 /**
- *  @type {{[key: string]: Controller }}
- *  Map of routes to controllers. The key is the hash name. The value is the controller class.
- *  @todo: Add routes + controllers here
+ * @type {{
+ * 	debug: boolean,
+ * 	baseTitle: string,
+ * 	appContainerID: string,
+ * 	routes: {[key: string]: {view: string, controller: Controller}},
+ * 	defaultRoute: string,
+ * 	locale: {
+ * 		switchSelectorID: string,
+ * 		configs: {[key: string]: string},
+ * 		images: {[key: string]: string},
+ * 	},
+ * 	localStorageKeys: {[key: string]: string},
+ * }}
 */
-const ROUTES = {
-	"home": HomeController,
-	"test": TestController,
-}
-// Default route if the hash is not found (preferred route).
-const DEFAULT_ROUTE = "home";
+const CONFIG = {
+	debug: false,
+	baseTitle: "Pong Game",
+	appContainerID: "app",
+
+	/**
+	 *  Routing map. The key is the hash (without the #) name. The value is an object with the following properties:
+	 *  view: view file name (without the .html extension)
+	 *  controller: Controller
+	 *  @todo: Add routes + controllers here
+	*/
+	routes: {
+		"": {
+			view: "home",
+			controller: HomeController,
+		},
+		"test": {
+			view: "test",
+			controller: TestController,
+		},
+	},
+	// Default route if the hash is not found.
+	defaultRoute: "",
+
+	locale: {
+		switchSelectorID: "locale-switch",
+		configs: {
+			en: "i18n/en.json",
+			it: "i18n/it.json",
+			uk: "i18n/uk.json",
+		},
+		images: {
+			en: "assets/flags/us.png",
+			it: "assets/flags/it.png",
+			uk: "assets/flags/ua.png",
+		}
+	},
+
+	localStorageKeys: {
+		locale: "locale",
+	},
+};
 
 //-----------------------------------------------------------------------------
 
 (async function () {
 	// I18N setup & load
-	// $.i18n.debug = true;
-	$.i18n().load(LOCALE_URLS);
+	$.i18n.debug = CONFIG.debug;
+	$.i18n().load(CONFIG.locale.configs);
 
-	function setLocale(locale="en"){
-		if (!Object.keys(LOCALE_URLS).includes(locale)) {
+	function setLocale(locale = "en") {
+		if (!Object.keys(CONFIG.locale.configs).includes(locale)) {
 			console.error("Unknown locale: " + locale);
 			locale = "en";
 		}
 		$.i18n().locale = locale;
-		localStorage.setItem(LOCAL_STORAGE_KEYS.locale, locale);
+		localStorage.setItem(CONFIG.localStorageKeys.locale, locale);
 		$(document.body).i18n();
-		const img = $(`#${LOCALE_SWITCH_ID} .selected-flag-img`);
-		if (img.attr("src") != LOCALE_IMAGES[locale]){
-			img.attr("src", LOCALE_IMAGES[locale]);
+		const img = $(`#${CONFIG.locale.switchSelectorID} .selected-flag-img`);
+		if (img.attr("src") != CONFIG.locale.images[locale]) {
+			img.attr("src", CONFIG.locale.images[locale]);
 		}
 		img.removeClass("d-none");
 	}
-	let savedLocale = localStorage.getItem(LOCAL_STORAGE_KEYS.locale);
+	let savedLocale = localStorage.getItem(CONFIG.localStorageKeys.locale);
 	setLocale(savedLocale);
 
-	$(`#${LOCALE_SWITCH_ID} .lang-item`).on("click", (el) => {
+	$(`#${CONFIG.locale.switchSelectorID} .lang-item`).on("click", (el) => {
 		const $el = $(el.currentTarget);
 		setLocale($el.data("lang"));
 	});
@@ -82,66 +108,48 @@ const DEFAULT_ROUTE = "home";
 		return view;
 	}
 
-	// TODO: add better error handling
-	function fetchText(path) {
-		return fetch(path)
-		.then(res => {
-			if (!res.ok) {
-				throw new Error(`HTTP error!: ${res}`);
-			}
-			return res.text();
-		})
-		.catch(err => {
-			alert("An error occurred. Reload the page please: "); console.error(err);
-			return null;
-		});
-	}
 
 	async function loadView(hashName) {
-		if (!hashName || !(hashName in ROUTES)) {
-			hashName = DEFAULT_ROUTE;
-		}
-
-		var viewFile = `/src/views/${hashName}.html`;
-
-		const view = (hashName in ROUTES) ? await fetchText(viewFile) : null;
-
-		if (!view) {
-			if (hashName != DEFAULT_ROUTE) {
-				window.location.hash = DEFAULT_ROUTE;
-			}
+		const selectedRoute = CONFIG.routes[hashName];
+		const isValidRoute = hashName in CONFIG.routes && selectedRoute;
+		if (!isValidRoute) {
+			if (hashName == CONFIG.defaultRoute) return;
+			console.warn(`Unknown route "${hashName}" - redirecting to "#${CONFIG.defaultRoute}"`);
+			window.location.hash = CONFIG.defaultRoute;
 			return;
 		}
 
-		$(`#${APP_CONTAINER_ID}`).html(view);
-		$(document.body).i18n();
-
-		const script = loadScript(hashName);
-
-		if (script) {
-			if (script.titleSuffix) {
-				document.title = `${APP_BASE_TITLE} - ${script.titleSuffix}`;
+		try {
+			let viewFile = selectedRoute.view;
+			if (!viewFile.endsWith(".html")){
+				viewFile += ".html";
 			}
-			script.init();
-		} else {
-			document.title = `${APP_BASE_TITLE}`;
-			if (!(hashName in ROUTES)) {
-				console.warn("No script found for #" + hashName);
+
+			const response = await fetch(`/src/views/${viewFile}`);
+			if (!response.ok){
+				throw new Error(`Failed to fetch view "${hashName}"`);
 			}
+			const viewHTML = await response.text();
+			$(`#${CONFIG.appContainerID}`).html(viewHTML);
+
+			$(document.body).i18n();
+
+			const controller = selectedRoute.controller ? new selectedRoute.controller() : null;
+			document.title = CONFIG.baseTitle;
+			if (controller){
+				if (controller.titleSuffix) {
+					document.title = `${CONFIG.baseTitle} - ${controller.titleSuffix}`;
+				}
+				controller.init();
+			}
+
+		} catch (err) {
+			// TODO: add better error handling
+			console.warn(`Failed to load route "${hashName}". Error: `, err);
+			alert("An error occurred. Reload the page please");
+			return;
 		}
-	}
-	/**
-	 * @param {string} hashName
-	 * @returns {Controller | null}
-	 */
-	function loadScript(hashName) {
-		if (!hashName || !(hashName in ROUTES)) {
-			hashName = DEFAULT_ROUTE;
-		}
-		if (ROUTES[hashName]) {
-			return new ROUTES[hashName]();
-		}
-		return null;
+
 	}
 
 	window.onhashchange = function () {
@@ -164,7 +172,7 @@ const DEFAULT_ROUTE = "home";
 	};
 
 	window.onload = function () {
-		document.title = APP_BASE_TITLE;
+		document.title = CONFIG.baseTitle;
 		$(window).trigger("hashchange");
 	};
 }());
